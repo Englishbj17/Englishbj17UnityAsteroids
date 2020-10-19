f84678c7a6f797f7dfdf91275035865bdb2406fc
Englishbj17 <60582673+Englishbj17@users.noreply.github.com>
2020-10-20 10:25:51 +1300

    forgot

---
+++ Assets/EuclideanTorus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class EuclideanTorus : MonoBehaviour {
+ 
+    // Update is called once per frame
+    void Update () {
+ 
+        // Teleport the game object
+        if(transform.position.x > 9){
+ 
+            transform.position = new Vector3(-208, transform.position.y, 0);
+ 
+        }
+        else if(transform.position.x < -208){
+            transform.position = new Vector3(9, transform.position.y, 0);
+        }
+ 
+        else if(transform.position.y > 430){
+            transform.position = new Vector3(transform.position.x, -6, 0);
+        }
+ 
+        else if(transform.position.y < -430){
+            transform.position = new Vector3(transform.position.x, 6, 0);
+        }
+    }
+}
+
